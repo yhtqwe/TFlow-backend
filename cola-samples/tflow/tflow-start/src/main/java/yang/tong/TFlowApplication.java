763a17d9706773868f1b03e7383ec966c83a9a56
yanghantong <2158179331@qq.com>
2024-05-27 20:23:08 +0800

    新功能(tflow):
    初建

---
+++ cola-samples/tflow/tflow-start/src/main/java/yang/tong/TFlowApplication.java
@@ -0,0 +1,17 @@
+package yang.tong;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: yanghantong
+ * @time: 2024/5/27 19:34
+ */
+@SpringBootApplication
+public class TFlowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TFlowApplication.class);
+    }
+}
